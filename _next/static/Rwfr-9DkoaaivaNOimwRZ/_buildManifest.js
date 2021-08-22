28c284cb02d82eda6d894d62bab3d2fd2c65ae47
thejayduck <therealjayduck@gmail.com>
2021-08-22 19:56:30 +0000

    Deploying to gh-pages from  @ c7f5d2026ea41e8a62ba0a6fc97f221d7ff45ef8 🚀

---
+++ _next/static/Rwfr-9DkoaaivaNOimwRZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/80-fa9e5c0fb1c090594f2e.js","static/css/f8107c243d1057f22d9e.css","static/chunks/pages/index-c785a096819c6d81944a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
