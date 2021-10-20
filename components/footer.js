9e1df2b570a0bde28bb6891d0666ac7ca6e11e29
thejayduck <ardafevzi.armutcu@gmail.com>
2021-10-21 00:22:26 +0300

    Updates Interface

--- components/footer.js
+++ components/footer.js
@@ -3,12 +3,12 @@ import styles from '@styles/Footer.module.scss'
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <div className={styles.container}>
+            <div className={`${styles.container} flex flexColumn`}>
                 <span className={styles.title}>Arda Fevzi Armutcu</span>
 
-                <ul className={styles.social}>
-                    <li><a aria-label="Github" target="_blank" title="Github" href="https://github.com/thejayduck" rel="noreferrer"><i className={"bx bxl-github"} /></a></li>
-                    <li><a aria-label="E-Mail" title="E-Mail" href="mailto:ardafevzi.armutcu@gmail.com"><i className={"bx bxs-envelope"} /></a></li>
+                <ul className={`${styles.social}`}>
+                    <li><a aria-label="Github" target="_blank" title="Github" href="https://github.com/thejayduck" rel="noreferrer">Github</a></li>
+                    <li><a aria-label="E-Mail" title="E-Mail" href="mailto:ardafevzi.armutcu@gmail.com">Contact</a></li>
                 </ul>
             </div>
         </footer>
